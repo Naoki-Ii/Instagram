bdf38902657dc7173bcc8c20a4b89e7104008312
Naoki.Ii <naoki1230f@gmail.com>
2020-03-03 21:26:09 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by NAOKI II on 2020/03/03.
+//  Copyright © 2020 NAOKI.II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
